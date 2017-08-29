b4039a5ba9d2740f30ef57285b196aa1e28c0a7f
Juntakan <59030139@kmitl.ac.th>
2017-08-29 14:49:47 +0700

    4th Year semester 2

--- EngEdu/EngEdu/Program.cs
+++ EngEdu/EngEdu/Program.cs
@@ -17,6 +17,7 @@ namespace EngEdu
             Console.WriteLine("Year 2561 semester 1, I");
             Console.WriteLine("Year 2561 semester 2, I");
             Console.WriteLine("Year 2562 semester 1, I");
+            Console.WriteLine("Year 2562 semester 2, I");
         }
     }
 }
